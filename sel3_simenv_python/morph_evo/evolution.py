5ab460ed94057dfdfc148e9d72360d281e311b3a
thordossche <dossche.thor@gmail.com>
2022-02-25 16:56:10 +0100

    evolution

---
+++ sel3_simenv_python/morph_evo/evolution.py
@@ -0,0 +1,62 @@
+from environment.environment import SimEnv
+from genetic_encoding import Genome
+from tqdm import tqdm
+from itertools import count
+from evaluator import Evaluator
+from logger import Logger
+import numpy as np
+import ray
+from ray.util import ActorPool
+import psutil
+
+def calculate_fitness(genome: Genome) -> float:
+    return genome.workspace.calculate_coverage()
+
+def evolution():
+    GENERATIONS = 100 
+    ENV_PATH = 'environment/unity_environment/simenv.x86_64'
+    # MU = # parents
+    # LAMBDA = # children
+    MU, LAMBDA = 5, 5
+    NUM_CORES = psutil.cpu_count()
+
+    genome_indexer = count(0)
+
+    evaluators = [Evaluator.remote(ENV_PATH, use_graphics=False) for _ in range(NUM_CORES)]
+    pool = ActorPool(evaluators)
+
+    logger = Logger()
+
+    parents, parent_fitnesses = [], []
+    children = [Genome(next(genome_indexer)) for _ in range(LAMBDA)]
+
+    for generation in tqdm(range(GENERATIONS), desc='Generation'):
+        # Evaluate children
+        children = list(pool.map_unordered(lambda evaluator, genome: evaluator.eval_genome.remote(genome), children))
+
+        children_fitnesses = []
+        for child in children:
+            children_fitnesses.append(calculate_fitness(child))
+
+        # Combine children and parents in one population
+
+        population = children + parents
+        population_fitnesses = children_fitnesses + parent_fitnesses
+
+        # Selection
+        parent_indices = np.argsort(population_fitnesses)[-MU:]
+        parents = [population[i] for i in parent_indices]
+        parent_fitnesses = [population_fitnesses[i] for i in parent_indices]
+
+        # create new children from selected parents
+        children = []
+        parent_index = 0
+        while len(children) < LAMBDA:
+            parent = parents[parent_index]
+            parent_index = (parent_index + 1) % MU
+
+            child = Genome(next(genome_indexer), parent_genome=parent)
+            children.append(child)
+
+        logger.log(generation, parents)
+
