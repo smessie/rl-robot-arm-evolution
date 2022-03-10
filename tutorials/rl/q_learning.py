767848b0281ed799adb2eed282e433eaf81ee64f
klagoeth <klaas.goethals@ugent.be>
2022-03-10 11:26:00 +0100

    fix: pylint formatting in RL tutorial

--- tutorials/rl/q_learning.py
+++ tutorials/rl/q_learning.py
@@ -1,15 +1,13 @@
 import pickle
 import random
-from typing import Tuple, Set
+import xml.etree.ElementTree as ET
+from typing import Set, Tuple
 
 import numpy as np
+from src.environment.environment import SimEnv
+from src.rl.logger import Logger
 from src.rl.q_table import QTable
 from tqdm import tqdm
-
-from src.environment.environment import SimEnv
-import xml.etree.ElementTree as ET
-from src.rl.logger import Logger
-
 
 
 class QLearner:
@@ -27,15 +25,16 @@ class QLearner:
     ALPHA = 0.1
     GAMMA = 0.99
 
-    def __init__(self, env_path: str, urdf_path: str, use_graphics: bool = False) -> None:
+    def __init__(self, env_path: str, urdf_path: str,
+                 use_graphics: bool = False) -> None:
         urdf = ET.tostring(ET.parse(urdf_path).getroot(), encoding='unicode')
         self.env = SimEnv(env_path, urdf, use_graphics=use_graphics)
         self.workspace = self._get_workspace()
 
-        self.q_table = QTable(  len(self.workspace) ** 2,
-                                len(self.ACTIONS),
-                                self.ALPHA,
-                                self.GAMMA)
+        self.q_table = QTable(len(self.workspace) ** 2,
+                              len(self.ACTIONS),
+                              self.ALPHA,
+                              self.GAMMA)
 
         self.logger = Logger()
 
@@ -44,24 +43,28 @@ class QLearner:
         return discretized_pos
 
     def _get_workspace(self) -> Set[Tuple[float, float]]:
-        with open ('src/environment/robot_workspace.pkl', "rb") as f:
-            workspace = pickle.load(f)
+        with open('src/environment/robot_workspace.pkl', "rb") as file:
+            workspace = pickle.load(file)
 
-        workspace = {tuple(self._discretize_position(np.array(pos))) for pos in workspace}
+        workspace = {tuple(self._discretize_position(np.array(pos)))
+                     for pos in workspace}
         return workspace
 
     def _generate_goal(self) -> np.ndarray:
         return np.array(random.sample(self.workspace, k=1)[0])
 
-    def _calculate_state(self, observations: np.ndarray, goal: np.ndarray) -> np.ndarray:
-        # [j0, j0x, j0y, j0z, j1, j1x, j1y, j1z, j2, j2x, j2y, j2z, ee_x, ee_y, ee_z]
+    def _calculate_state(self, observations: np.ndarray,
+                         goal: np.ndarray) -> np.ndarray:
+        # [j0, j0x, j0y, j0z, j1, j1x, j1y, j1z,
+        #  j2, j2x, j2y, j2z, ee_x, ee_y, ee_z]
         # [EEPOS, GOAL_y, GOAL_z]
         ee_pos = observations[13:15]
 
         ee_pos = self._discretize_position(ee_pos)
         return np.array([ee_pos[0], ee_pos[1], goal[0], goal[1]], dtype=int)
 
-    def _calculate_reward(self, prev_state: np.ndarray, new_state: np.ndarray, goal: np.ndarray) -> Tuple[float, bool]:
+    def _calculate_reward(self, prev_state: np.ndarray, new_state: np.ndarray,
+                          goal: np.ndarray) -> Tuple[float, bool]:
         prev_distance_from_goal = np.linalg.norm(prev_state[:2] - goal)
         new_distance_from_goal = np.linalg.norm(new_state[:2] - goal)
 
@@ -70,7 +73,8 @@ class QLearner:
 
         return prev_distance_from_goal - new_distance_from_goal, False
 
-    def learn(self, num_episodes: int = 10000, steps_per_episode: int = 500) -> None:
+    def learn(self, num_episodes: int = 10000,
+              steps_per_episode: int = 500) -> None:
         for episode in tqdm(range(num_episodes), desc='Q-Learning'):
             observations = self.env.reset()
             goal = self._generate_goal()
@@ -86,9 +90,10 @@ class QLearner:
                 # Execute the action in the environment
                 observations = self.env.step(actions)
                 new_state = self._calculate_state(observations, goal)
-                
+
                 # Calculate reward
-                reward, finished =  self._calculate_reward(state, new_state, goal)
+                reward, finished = self._calculate_reward(
+                    state, new_state, goal)
 
                 # QTable update
                 self.q_table.update(state, new_state, action_index, reward)
@@ -96,7 +101,8 @@ class QLearner:
                 episode_step += 1
                 state = new_state
 
-            self.logger.log_episode(episode, state, goal, episode_step, self.q_table)
+            self.logger.log_episode(
+                episode, state, goal, episode_step, self.q_table)
 
         self.env.close()
         self.save()
@@ -104,8 +110,7 @@ class QLearner:
     def predict(self, state: np.ndarray, stochastic: bool = False) -> int:
         if stochastic and np.random.rand() < self.EPSILON:
             return np.random.randint(len(self.ACTIONS))
-        else:
-            return self.q_table.lookup(state)
+        return self.q_table.lookup(state)
 
     def save(self):
         pass
@@ -113,8 +118,8 @@ class QLearner:
 
 if __name__ == "__main__":
 
-    env_path = "src/environment/unity_environment/simenv.x86_64"
-    urdf_path = "src/environment/robot.urdf"
+    ENV_PATH = "src/environment/unity_environment/simenv.x86_64"
+    URDF_PATH = "src/environment/robot.urdf"
 
-    model = QLearner(env_path, urdf_path, True)
+    model = QLearner(ENV_PATH, URDF_PATH, True)
     model.learn()
--- tutorials/rl/src/environment/environment.py
+++ tutorials/rl/src/environment/environment.py
@@ -1,14 +1,14 @@
+import os
 from pathlib import Path
 from typing import Tuple
 
-import os
 import gym
 import numpy as np
 from mlagents_envs.base_env import ActionTuple
 from mlagents_envs.environment import UnityEnvironment
-from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
-
-from src.environment.sidechannels.creationSC import CreationSC
+from mlagents_envs.side_channel.engine_configuration_channel import \
+    EngineConfigurationChannel
+from src.environment.sidechannels.creation_sc import CreationSC
 
 
 class SimEnv(gym.Env):
@@ -17,12 +17,15 @@ class SimEnv(gym.Env):
     MAX_N_MODULES = 10
     JOINT_ANGLE_STEP = 10
 
-    def __init__(self, env_path: str, urdf: str, use_graphics: bool, worker_id: int = 0) -> None:
-        super(SimEnv, self).__init__()
+    def __init__(self, env_path: str, urdf: str, use_graphics: bool,
+                 worker_id: int = 0) -> None:
+        super().__init__()
 
-        assert Path(env_path).exists(), f"Given environment file path does not exist: {env_path}\n" \
-                                        f"Make sure this points to the environment executable " \
-                                        f"that you can download from Ufora."
+        assert Path(env_path).exists(), (
+            f"Given environment file path does not exist: {env_path}\n"
+            f"Make sure this points to the environment executable "
+            f"that you can download from Ufora."
+        )
 
         self.env_path = env_path
         self.urdf = urdf
@@ -54,18 +57,20 @@ class SimEnv(gym.Env):
         return creation_sc, env
 
     def _get_unity_observations(self) -> np.ndarray:
-        decision_steps, terminal_steps = self.u_env.get_steps(self.behavior_name)
+        decision_steps, _ = self.u_env.get_steps(
+            self.behavior_name)
         return decision_steps.obs[0][0]
 
     def _set_unity_actions(self, actions: np.ndarray) -> None:
         # Assume the user of this already mapped the actions to -1, 0 or 1
-        #   We want to allow both discrete and continuous actions to stay as generic as possible
+        #   We want to allow both discrete and continuous actions
+        #    to stay as generic as possible
         actions = np.pad(actions, (0, self.MAX_N_MODULES - len(actions)))
         actions = actions[None, :]
         self.u_env.set_actions(self.behavior_name, action=ActionTuple(actions))
 
-    def step(self, actions: np.ndarray) -> np.ndarray:
-        self._set_unity_actions(actions)
+    def step(self, action: np.ndarray) -> np.ndarray:
+        self._set_unity_actions(action)
         self.u_env.step()
 
         observations = self._get_unity_observations()
@@ -86,31 +91,30 @@ if __name__ == '__main__':
     import xml.etree.ElementTree as ET
 
     # make absolute paths to avoid file-not-found errors
-    here = os.path.dirname(os.path.abspath(__file__))
-    urdf_filename = os.path.join(here, 'robot.urdf')
-    env_filename = os.path.join(here, 'unity_environment/simenv.x86_64')
+    HERE = os.path.dirname(os.path.abspath(__file__))
+    URDF_FILENAME = os.path.join(HERE, 'robot.urdf')
+    ENV_FILENAME = os.path.join(HERE, 'unity_environment/simenv.x86_64')
 
-    PATH_TO_YOUR_UNITY_EXECUTABLE = env_filename 
-    urdf = ET.tostring(ET.parse(urdf_filename).getroot(), encoding='unicode')
+    PATH_TO_YOUR_UNITY_EXECUTABLE = ENV_FILENAME
+    URDF = ET.tostring(ET.parse(URDF_FILENAME).getroot(), encoding='unicode')
 
-    env = SimEnv(env_path=PATH_TO_YOUR_UNITY_EXECUTABLE,
-                 urdf=urdf,
+    ENV = SimEnv(env_path=PATH_TO_YOUR_UNITY_EXECUTABLE,
+                 urdf=URDF,
                  use_graphics=True)
 
-    obs = env.reset()
+    obs = ENV.reset()
     for _ in range(1000):
         # obs -> MODEL > actions
-        actions = np.random.rand(3)
+        ACTIONS = np.random.rand(3)
 
-        actions = (actions - 0.5) * 2  # Map to [-1, 1] range
-        actions[actions > 0.2] = 1
-        actions[actions < -0.2] = -1
-        actions[np.abs(actions) < 0.2] = 0
+        ACTIONS = (ACTIONS - 0.5) * 2  # Map to [-1, 1] range
+        ACTIONS[ACTIONS > 0.2] = 1
+        ACTIONS[ACTIONS < -0.2] = -1
+        ACTIONS[np.abs(ACTIONS) < 0.2] = 0
 
         # We will not actuate the first joint in this tutorial
-        actions[0] = 0.
+        ACTIONS[0] = 0.
 
-        obs = env.step(actions)
+        obs = ENV.step(ACTIONS)
 
-    env.close()
-
+    ENV.close()
