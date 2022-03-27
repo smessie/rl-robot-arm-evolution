c99b6f379afef6a4f38cc394d594f5fac30f5ab0
thordossche <thordossche@users.noreply.github.com>
2022-03-27 08:40:24 +0000

    deploy: be57a1931aa5256ab89afa22686f121ec3f35981

--- search/classes_3.js
+++ search/classes_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['qlearner_0',['QLearner',['../classq__learning_1_1QLearner.html',1,'q_learning']]],
-  ['qtable_1',['QTable',['../classsrc_1_1rl_1_1q__table_1_1QTable.html',1,'src::rl::q_table']]]
+  ['parameters_0',['Parameters',['../classsrc_1_1morphevo_1_1parameters_1_1Parameters.html',1,'src::morphevo::parameters']]]
 ];
