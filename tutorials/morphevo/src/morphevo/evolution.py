7950ddef515d4da93926f21da0b44264cf10b8ca
thordossche <dossche.thor@gmail.com>
2022-03-27 11:23:50 +0200

    linitn

--- tutorials/morphevo/src/morphevo/evolution.py
+++ tutorials/morphevo/src/morphevo/evolution.py
@@ -8,11 +8,6 @@ from morphevo.evaluator import Evaluator
 from morphevo.genetic_encoding import Genome
 from morphevo.logger import Logger
 
-GENERATIONS = 100
-# MU = # parents
-# LAMBDA = # children
-MU, LAMBDA = 5, 5
-
 
 def calculate_fitness(genome: Genome) -> float:
     return genome.workspace.calculate_coverage()
@@ -30,7 +25,7 @@ def evolution(evolution_parameters):
     parents, parent_fitnesses = [], []
     children = [Genome(next(genome_indexer)) for _ in range(evolution_parameters.LAMBDA)]
 
-    for generation in tqdm(range(GENERATIONS), desc='Generation'):
+    for generation in tqdm(range(evolution_parameters.GENERATIONS), desc='Generation'):
         # Evaluate children
         children = list(pool.map_unordered(
             lambda evaluator, genome: evaluator.eval_genome.remote(genome), children))
@@ -52,7 +47,7 @@ def evolution(evolution_parameters):
         # create new children from selected parents
         children = []
         parent_index = 0
-        while len(children) < LAMBDA:
+        while len(children) < evolution_parameters.LAMBDA:
             parent = parents[parent_index]
             parent_index = (parent_index + 1) % evolution_parameters.MU
 
--- tutorials/morphevo/src/run.py
+++ tutorials/morphevo/src/run.py
@@ -9,7 +9,7 @@ from morphevo.parameters import Parameters
 if __name__ == '__main__':
     if not exists(sys.argv[1]):
         print(f"Configfile '{sys.argv[1]}' does not exist.")
-        exit()
+        sys.exit()
     evolution_parameters = Parameters(sys.argv[1])
     ray.init(log_to_driver=False, logging_level=logging.WARNING)
     evolution(evolution_parameters)
