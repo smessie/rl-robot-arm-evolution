98756999aa7730085874a44f021904b8674b0a22
thordossche <dossche.thor@gmail.com>
2022-03-28 09:29:04 +0200

    chore: add error for morphevo run without args

--- tutorials/morphevo/src/run.py
+++ tutorials/morphevo/src/run.py
@@ -7,6 +7,9 @@ from morphevo.evolution import evolution
 from morphevo.parameters import Parameters
 
 if __name__ == '__main__':
+    if not len(sys.argv) > 1:
+        print("Something wrong with program arguments")
+        sys.exit()
     if not exists(sys.argv[1]):
         print(f"Configfile '{sys.argv[1]}' does not exist.")
         sys.exit()
