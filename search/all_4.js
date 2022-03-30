af54d8bed2243029efb1301abae2496d8485d0a5
Iwijn <Iwijn@users.noreply.github.com>
2022-03-30 13:45:39 +0000

    deploy: bc3d3eaab9893ba6737dcce88a2990c53298c405

--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['qlearner_0',['QLearner',['../classq__learning_1_1QLearner.html',1,'q_learning.QLearner'],['../classq__learning__tutorial_1_1QLearner.html',1,'q_learning_tutorial.QLearner']]],
+  ['qlearner_0',['QLearner',['../classq__learning_1_1QLearner.html',1,'q_learning.QLearner'],['../classq__learning__tutorial_1_1QLearner.html',1,'q_learning_tutorial.QLearner'],['../classsrc_1_1rl_1_1q__learning_1_1QLearner.html',1,'src.rl.q_learning.QLearner']]],
   ['qtable_1',['QTable',['../classsrc_1_1rl_1_1q__table_1_1QTable.html',1,'src::rl::q_table']]]
 ];
