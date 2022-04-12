16fdde58f00338d0c6161c8fd232269c5b7f047b
klaasg <klaasg@users.noreply.github.com>
2022-04-12 09:14:09 +0000

    deploy: 048e112be418a09c823bd082918912ff632a0494

--- search/all_c.js
+++ search/all_c.js
@@ -3,6 +3,7 @@ var searchData=
   ['readme_0',['README',['../md_poster_README.html',1,'']]],
   ['readme_1',['Readme',['../classReadme.html',1,'']]],
   ['readmeeditor_2',['ReadmeEditor',['../classReadmeEditor.html',1,'']]],
-  ['robotspecification_3',['RobotSpecification',['../classDefaultNamespace_1_1RobotSpecification.html',1,'DefaultNamespace']]],
-  ['rotationspec_4',['RotationSpec',['../classDefaultNamespace_1_1RotationSpec.html',1,'DefaultNamespace']]]
+  ['robotinfo_3',['RobotInfo',['../classCreationSC_1_1RobotInfo.html',1,'CreationSC']]],
+  ['robotspecification_4',['RobotSpecification',['../classDefaultNamespace_1_1RobotSpecification.html',1,'DefaultNamespace']]],
+  ['rotationspec_5',['RotationSpec',['../classDefaultNamespace_1_1RotationSpec.html',1,'DefaultNamespace']]]
 ];
--- search/classes_b.js
+++ search/classes_b.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['readme_0',['Readme',['../classReadme.html',1,'']]],
   ['readmeeditor_1',['ReadmeEditor',['../classReadmeEditor.html',1,'']]],
-  ['robotspecification_2',['RobotSpecification',['../classDefaultNamespace_1_1RobotSpecification.html',1,'DefaultNamespace']]],
-  ['rotationspec_3',['RotationSpec',['../classDefaultNamespace_1_1RotationSpec.html',1,'DefaultNamespace']]]
+  ['robotinfo_2',['RobotInfo',['../classCreationSC_1_1RobotInfo.html',1,'CreationSC']]],
+  ['robotspecification_3',['RobotSpecification',['../classDefaultNamespace_1_1RobotSpecification.html',1,'DefaultNamespace']]],
+  ['rotationspec_4',['RotationSpec',['../classDefaultNamespace_1_1RotationSpec.html',1,'DefaultNamespace']]]
 ];
