f6435697ba0ec43bf9dccdeff217fbb512bbf24c
thordossche <dossche.thor@gmail.com>
2022-04-13 14:57:33 +0200

    cleanup + fix config

--- src/configs/environment_example.py
+++ src/configs/environment_example.py
@@ -2,5 +2,5 @@ import psutil
 
 # Copy this file to env.py and then adjust all settings to your wishes, in that env.py file.
 PATH_TO_UNITY_EXECUTABLE = '../build/simenv.x86_64'
-USE_GRAPHICS = True
+USE_GRAPHICS = False
 NUM_CORES = psutil.cpu_count()
