d5f324700975191f017518c211d9ab64111990c6
freyavs <freyavs@users.noreply.github.com>
2022-04-20 13:50:46 +0000

    deploy: e9d5f9ce03d99bb7e5048d521b4d0bb0ac1dcbd9

--- python/search/all_5.js
+++ python/search/all_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['moduletype_0',['ModuleType',['../classmorphevo_1_1genetic__encoding_1_1ModuleType.html',1,'morphevo::genetic_encoding']]]
+  ['logger_0',['Logger',['../classmorphevo_1_1logger_1_1Logger.html',1,'morphevo.logger.Logger'],['../classrl_1_1logger_1_1Logger.html',1,'rl.logger.Logger']]]
 ];
