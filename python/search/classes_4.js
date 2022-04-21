26b790c529827be073cd32a84511a1674cd7d9a4
klaasg <klaasg@users.noreply.github.com>
2022-04-21 08:46:35 +0000

    deploy: 4b908417a250d3c61213c61a62400dcc5d5c0413

--- python/search/classes_4.js
+++ python/search/classes_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['logger_0',['Logger',['../classmorphevo_1_1logger_1_1Logger.html',1,'morphevo.logger.Logger'],['../classrl_1_1logger_1_1Logger.html',1,'rl.logger.Logger']]]
+  ['parameters_0',['Parameters',['../classmorphevo_1_1parameters_1_1Parameters.html',1,'morphevo::parameters']]]
 ];
