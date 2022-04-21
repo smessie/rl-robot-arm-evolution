b31b35791bdc1e9ef16386a123e8da18df8d316b
freyavs <freyavs@users.noreply.github.com>
2022-04-21 15:08:09 +0000

    deploy: 065ce89bda01e89bc111db6c1050957da9ab8946

--- python/search/classes_0.js
+++ python/search/classes_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5f_5fconfig_0',['__Config',['../classmorphevo_1_1config_1_1Config_1_1____Config.html',1,'morphevo::config::Config']]]
+  ['creationsc_0',['CreationSC',['../classenvironment_1_1sidechannels_1_1creation__sc_1_1CreationSC.html',1,'environment::sidechannels::creation_sc']]]
 ];
--- python/search/searchdata.js
+++ python/search/searchdata.js
@@ -1,7 +1,7 @@
 var indexSectionsWithContent =
 {
-  0: "_cdeglmqrsuw",
-  1: "_cdeglmqrsuw",
+  0: "_cdeglmpqrsuw",
+  1: "cdeglmpqrsuw",
   2: "_u"
 };
 
