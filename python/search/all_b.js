4085533aadedf85b32edf1a01825006065a91186
smessie <smessie@users.noreply.github.com>
2022-04-21 09:11:29 +0000

    deploy: 7cc1b104d8b46bb9f5f380ce3873fd8385fa627d

--- python/search/all_b.js
+++ python/search/all_b.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['update_0',['update',['../classrl_1_1dqn_1_1DQN.html#a70722326d79747510dbc502374c89792',1,'rl::dqn::DQN']]],
-  ['urdfgenerator_1',['URDFGenerator',['../classmorphevo_1_1urdf__generator_1_1URDFGenerator.html',1,'morphevo::urdf_generator']]]
+  ['visualspec_0',['VisualSpec',['../classDefaultNamespace_1_1VisualSpec.html',1,'DefaultNamespace']]]
 ];
--- python/search/all_c.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['workspace_0',['Workspace',['../classmorphevo_1_1workspace_1_1Workspace.html',1,'morphevo::workspace']]],
-  ['workspacenotfounderror_1',['WorkspaceNotFoundError',['../classmorphevo_1_1workspace_1_1WorkspaceNotFoundError.html',1,'morphevo::workspace']]]
-];
