4c9093dd25d37e8d50d0ebe619d924682822bfcc
Iwijn <Iwijn@users.noreply.github.com>
2022-04-27 14:22:53 +0000

    deploy: a11b95126541e7eac3509da4935eb58e738e23e5

--- python/search/all_0.js
+++ python/search/all_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['_5f_5fconfig_0',['__Config',['../classmorphevo_1_1config_1_1Config_1_1____Config.html',1,'morphevo::config::Config']]],
-  ['_5f_5finit_5f_5f_1',['__init__',['../classmorphevo_1_1workspace_1_1Workspace.html#ac4d7c75e10503832bb9a7cd3bea30b7a',1,'morphevo::workspace::Workspace']]]
+  ['_5f_5finit_5f_5f_0',['__init__',['../classmorphevo_1_1workspace_1_1Workspace.html#ac4d7c75e10503832bb9a7cd3bea30b7a',1,'morphevo.workspace.Workspace.__init__()'],['../classrl_1_1deep__q__learning_1_1DeepQLearner.html#a0e14039279e46da61436a27f9728e3e6',1,'rl.deep_q_learning.DeepQLearner.__init__()']]]
 ];
