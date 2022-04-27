0a790f4660ca7478101646686c1b14b77a2b660f
thordossche <thordossche@users.noreply.github.com>
2022-04-27 14:15:40 +0000

    deploy: ad1ea532639ff6b2dec180acbccb636841cb19ab

--- python/search/classes_3.js
+++ python/search/classes_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['genome_0',['Genome',['../classmorphevo_1_1genetic__encoding_1_1Genome.html',1,'morphevo::genetic_encoding']]],
-  ['goalsc_1',['GoalSC',['../classenvironment_1_1sidechannels_1_1goal__sc_1_1GoalSC.html',1,'environment::sidechannels::goal_sc']]]
+  ['evaluator_0',['Evaluator',['../classmorphevo_1_1evaluator_1_1Evaluator.html',1,'morphevo::evaluator']]]
 ];
