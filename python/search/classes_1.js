bfdf7a8b8d7bf5011ee2163f73d49ab0fa02d70d
Iwijn <Iwijn@users.noreply.github.com>
2022-05-05 09:49:13 +0000

    deploy: 2ccee385a8edf3de998c9f0d567cd1106afbce39

--- python/search/classes_1.js
+++ python/search/classes_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['config_0',['Config',['../classmorphevo_1_1config_1_1Config.html',1,'morphevo::config']]],
+  ['config_0',['Config',['../classutil_1_1config_1_1Config.html',1,'util::config']]],
   ['creationsc_1',['CreationSC',['../classenvironment_1_1sidechannels_1_1creation__sc_1_1CreationSC.html',1,'environment::sidechannels::creation_sc']]]
 ];
