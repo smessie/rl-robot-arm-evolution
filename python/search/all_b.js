9de761c31198b92bbc434a97221e61ea08076d90
Iwijn <Iwijn@users.noreply.github.com>
2022-05-05 08:55:59 +0000

    deploy: 441a59d828cb4c3e29c81bf2995319946b1bc30e

--- python/search/all_b.js
+++ python/search/all_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['wallsc_0',['WallSC',['../classenvironment_1_1sidechannels_1_1wall__sc_1_1WallSC.html',1,'environment::sidechannels::wall_sc']]],
-  ['workspace_1',['Workspace',['../classmorphevo_1_1workspace_1_1Workspace.html',1,'morphevo::workspace']]],
-  ['workspacenotfounderror_2',['WorkspaceNotFoundError',['../classmorphevo_1_1workspace_1_1WorkspaceNotFoundError.html',1,'morphevo::workspace']]]
+  ['workspace_0',['Workspace',['../classmorphevo_1_1workspace_1_1Workspace.html',1,'morphevo::workspace']]],
+  ['workspacenotfounderror_1',['WorkspaceNotFoundError',['../classmorphevo_1_1workspace_1_1WorkspaceNotFoundError.html',1,'morphevo::workspace']]],
+  ['workspaceparameters_2',['WorkspaceParameters',['../classmorphevo_1_1workspace__parameters_1_1WorkspaceParameters.html',1,'morphevo::workspace_parameters']]]
 ];
--- python/search/all_c.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['wallbuilder_0',['WallBuilder',['../classWallBuilder.html',1,'']]],
-  ['wallsc_1',['WallSC',['../classWallSC.html',1,'']]]
-];
