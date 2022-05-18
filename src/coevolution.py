2be5760dd452974fcc2d08ff93baaa31600418be
smessie <smessie@smessie.com>
2022-05-18 14:40:30 +0200

    feat: Save best genome of coevolution

--- src/coevolution.py
+++ src/coevolution.py
@@ -1,6 +1,11 @@
+import locale
+import time
+from xml.dom import minidom
+
 from morphevo.evolution import (evolution, mutate, mutate_with_crossover,
                                 selection, selection_succes_rate)
 from rl.deep_q_learning import train
+from util.arm import Arm
 from util.config import get_config
 from util.util import generate_arms
 
@@ -19,3 +24,14 @@ def start_coevolution():
 
         # mutate 8 parents to get 32 new children
         children = mutate(mutate_with_crossover, parents)
+
+    save_best_genome(parents[0])
+
+
+def save_best_genome(arm: Arm):
+    filename = (f'output/{int(time.time())}-mu_{get_config().evolution_parents}' +
+                f'-lambda_{get_config().evolution_children}-gamma_{get_config().gamma}-final_rl_best.xml')
+
+    xml_str = minidom.parseString(arm.urdf).toprettyxml(indent="    ")
+    with open(filename, "w", encoding=locale.getpreferredencoding(False)) as f:
+        f.write(xml_str)
