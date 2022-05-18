3ae73d6dcddae601ca461a3839cdc7dfc9c09a7c
smessie <smessie@smessie.com>
2022-05-18 17:21:47 +0200

    fix: Fix step_until_target_angles actions definer

--- src/util/config.py
+++ src/util/config.py
@@ -22,6 +22,7 @@ class Config:
                 self.evolution_parents = morphevo['evolution_parents']
                 self.evolution_children = morphevo['evolution_children']
                 self.crossover_children = morphevo['crossover_children']
+                self.sample_size = morphevo['sample_size']
                 self.workspace_parameters = self.parse_workspace_parameters(morphevo)
 
             if 'rl' in config:
