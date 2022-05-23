f8b0d07d9156920d53ba67a5ccee95940842e878
thordossche <dossche.thor@gmail.com>
2022-05-23 14:43:27 +0200

    fix: mutationbug

--- src/util/arm.py
+++ src/util/arm.py
@@ -7,7 +7,7 @@ from rl.dqn import DQN
 class Arm:
     def __init__(self, genome: Optional[Genome] = None) -> None:
         self.urdf_path: str = ""
-        self.genome: Genome = genome if genome else Genome()
+        self.genome: Genome = Genome(genome) if genome else Genome()
         self.rl_model: DQN = None
         self.success_rate: float = None
 
