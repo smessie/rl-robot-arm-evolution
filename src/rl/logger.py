7013ce831458604846a93e9ca516e5a7e7ea807e
freyavs <freyavanspeybroeck@outlook.com>
2022-05-24 10:18:23 +0200

    demo network

--- src/rl/logger.py
+++ src/rl/logger.py
@@ -7,7 +7,7 @@ import wandb
 
 class Logger:
     def __init__(self):
-        wandb.init(project='sel3-rl')
+        wandb.init(project='sel3-rl-demo')
 
     # pylint: disable-msg=too-many-arguments
     def log_episode(self, episode: int, final_state: np.ndarray,
