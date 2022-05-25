0896ae1d46d267ef1ead02e4dbe02463788fda59
thordossche <thordossche@users.noreply.github.com>
2022-05-25 09:28:44 +0000

    deploy: 0c00b343aa6bcdb38ab70d86819b12044562ba09

--- unity/search/all_9.js
+++ unity/search/all_9.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['tiltingmodule_0',['TiltingModule',['../classDefaultNamespace_1_1TiltingModule.html',1,'DefaultNamespace']]],
-  ['tiltingspec_1',['TiltingSpec',['../classDefaultNamespace_1_1TiltingSpec.html',1,'DefaultNamespace']]]
+  ['sidechannelregistrator_0',['SideChannelRegistrator',['../classSideChannelRegistrator.html',1,'']]]
 ];
--- unity/search/all_b.js
+++ unity/search/all_b.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['wallbuilder_0',['WallBuilder',['../classWallBuilder.html',1,'']]],
-  ['wallsc_1',['WallSC',['../classWallSC.html',1,'']]],
-  ['workspacesc_2',['WorkspaceSC',['../classWorkspaceSC.html',1,'']]]
+  ['visualspec_0',['VisualSpec',['../classDefaultNamespace_1_1VisualSpec.html',1,'DefaultNamespace']]]
 ];
