0c00b343aa6bcdb38ab70d86819b12044562ba09
thordossche <dossche.thor@gmail.com>
2022-05-25 11:28:01 +0200

    refactor: environment config

--- src/configs/environment_example.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-# Copy this file to env.py and then adjust all settings to your wishes, in that env.py file.
-PATH_TO_UNITY_EXECUTABLE = '../build/simenv.x86_64'
-PATH_TO_ROBOT_URDF = "environment/robot.urdf"
-MORPHEVO_USE_GRAPHICS = False
-RL_USE_GRAPHICS_TRAINING = False
-RL_USE_GRAPHICS_TESTING = True
-NUM_CORES = psutil.cpu_count()
