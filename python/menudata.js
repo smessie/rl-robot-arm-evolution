16b17c69ba98830b5315cc04bb5d42ee4a936a9c
Iwijn <Iwijn@users.noreply.github.com>
2022-05-26 10:48:26 +0000

    deploy: 0bfeedede410e8299bdc74ec114228d8d976fd34

--- python/menudata.js
+++ python/menudata.js
@@ -29,5 +29,27 @@ var menudata={children:[
 {text:"Class Index",url:"classes.html"},
 {text:"Class Hierarchy",url:"hierarchy.html"},
 {text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html"},
-{text:"Functions",url:"functions_func.html"}]}]}]}
+{text:"All",url:"functions.html",children:[
+{text:"_",url:"functions.html#index__5F"},
+{text:"a",url:"functions.html#index_a"},
+{text:"c",url:"functions.html#index_c"},
+{text:"e",url:"functions.html#index_e"},
+{text:"g",url:"functions.html#index_g"},
+{text:"i",url:"functions.html#index_i"},
+{text:"l",url:"functions.html#index_l"},
+{text:"m",url:"functions.html#index_m"},
+{text:"p",url:"functions.html#index_p"},
+{text:"s",url:"functions.html#index_s"},
+{text:"u",url:"functions.html#index_u"}]},
+{text:"Functions",url:"functions_func.html",children:[
+{text:"_",url:"functions_func.html#index__5F"},
+{text:"a",url:"functions_func.html#index_a"},
+{text:"c",url:"functions_func.html#index_c"},
+{text:"e",url:"functions_func.html#index_e"},
+{text:"g",url:"functions_func.html#index_g"},
+{text:"i",url:"functions_func.html#index_i"},
+{text:"l",url:"functions_func.html#index_l"},
+{text:"m",url:"functions_func.html#index_m"},
+{text:"p",url:"functions_func.html#index_p"},
+{text:"s",url:"functions_func.html#index_s"},
+{text:"u",url:"functions_func.html#index_u"}]}]}]}]}
--- python/search/all_7.js
+++ python/search/all_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['logger_0',['Logger',['../classrl_1_1logger_1_1Logger.html',1,'rl::logger']]]
+  ['learn_0',['learn',['../classrl_1_1deep__q__learning_1_1DeepQLearner.html#ac3269afbe1871f7b4b26e34087c495a5',1,'rl::deep_q_learning::DeepQLearner']]],
+  ['logger_1',['Logger',['../classrl_1_1logger_1_1Logger.html',1,'rl::logger']]]
 ];
