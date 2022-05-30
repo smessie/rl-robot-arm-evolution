c98e243d8e2cc57032c3690519f6782d7c0e4d3b
Iwijn <iwijn.voeten@gmail.com>
2022-05-30 13:43:06 +0200

    docs: document arm.py

--- src/util/arm.py
+++ src/util/arm.py
@@ -5,7 +5,12 @@ from rl.dqn import DQN
 
 
 class Arm:
+    """! Class that represents an arm.
+    """
     def __init__(self, genome: Optional[Genome] = None) -> None:
+        """! The Arm class initializer
+        @param genome Representation of the arm as a phenotype
+        """
         self.urdf_path: str = ""
         self.genome: Genome = Genome(genome) if genome else Genome()
         self.rl_model: DQN = None
