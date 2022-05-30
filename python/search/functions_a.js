b5b28ad048515e8eb85ee7f5ee38c12465bf305b
Iwijn <Iwijn@users.noreply.github.com>
2022-05-30 09:48:18 +0000

    deploy: 2b8d4e9adbf9acaa6f7428cc9f7d082316939b65

--- python/search/functions_a.js
+++ python/search/functions_a.js
@@ -7,5 +7,6 @@ var searchData=
   ['selection_5ffitness_4',['selection_fitness',['../evolution_8py.html#a5e70461c23098357c3a8c2a9fe747afa',1,'morphevo::evolution']]],
   ['selection_5ffitness_5fdiversity_5',['selection_fitness_diversity',['../evolution_8py.html#a38aae5ab8626d716faf00354caf1582a',1,'morphevo::evolution']]],
   ['selection_5fsucces_5frate_6',['selection_succes_rate',['../evolution_8py.html#a74cbb6c3784dd4c68f0599eac044698d',1,'morphevo::evolution']]],
-  ['step_7',['step',['../classenvironment_1_1environment_1_1SimEnv.html#a3fa19baa10feb6a247e26153f89ef30a',1,'environment.environment.SimEnv.step()'],['../classrl_1_1deep__q__learning_1_1DeepQLearner.html#a805c5534dd3f5e18a0c004e2f68cd95f',1,'rl.deep_q_learning.DeepQLearner.step()']]]
+  ['send_5fbuild_5fcommand_7',['send_build_command',['../classenvironment_1_1sidechannels_1_1creation__sc_1_1CreationSC.html#a6c19a9dfaffa8d354a9650ca2cac1530',1,'environment::sidechannels::creation_sc::CreationSC']]],
+  ['step_8',['step',['../classenvironment_1_1environment_1_1SimEnv.html#a3fa19baa10feb6a247e26153f89ef30a',1,'environment.environment.SimEnv.step()'],['../classrl_1_1deep__q__learning_1_1DeepQLearner.html#a805c5534dd3f5e18a0c004e2f68cd95f',1,'rl.deep_q_learning.DeepQLearner.step()']]]
 ];
