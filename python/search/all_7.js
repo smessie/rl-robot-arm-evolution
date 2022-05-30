30fad69c6d0930a7826879e3955153c05bedd1a3
Iwijn <Iwijn@users.noreply.github.com>
2022-05-30 09:57:12 +0000

    deploy: 3b5fb817ff0c345b457b8053848d6f78ed22f92b

--- python/search/all_7.js
+++ python/search/all_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['learn_0',['learn',['../classrl_1_1deep__q__learning_1_1DeepQLearner.html#ac3269afbe1871f7b4b26e34087c495a5',1,'rl::deep_q_learning::DeepQLearner']]],
+  ['learn_0',['learn',['../classrl_1_1deep__q__learning_1_1DeepQLearner.html#a34f1cfc317e597422d521a4b9f89e965',1,'rl::deep_q_learning::DeepQLearner']]],
   ['logger_1',['Logger',['../classrl_1_1logger_1_1Logger.html',1,'rl::logger']]]
 ];
