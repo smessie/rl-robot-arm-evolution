b2f6714bdde52d0ecc422f1ed93cb06799a1489f
klaasg <klaasg@users.noreply.github.com>
2022-05-30 12:32:29 +0000

    deploy: 9a0b5b880bb731054c39c55a59cb5fecfe139849

--- unity/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['collectobservations_0',['CollectObservations',['../classAgentBrain.html#acece71a0024f97365d3e8fd7bb5920f7',1,'AgentBrain']]]
-];
--- unity/search/functions_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['onactionreceived_0',['OnActionReceived',['../classAgentBrain.html#af8d77b415d8b22a4b27de4e8f4400d57',1,'AgentBrain']]],
-  ['onepisodebegin_1',['OnEpisodeBegin',['../classAgentBrain.html#a9b94c91acd45f49d42307d3120113775',1,'AgentBrain']]]
-];
